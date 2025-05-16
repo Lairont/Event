f8703126f416d06b2d994e59a3665fc34065d6b4
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-16 12:34:01 +0300

    Add files via upload

---
+++ ConsoleApp18/Handler1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Handler1
+    {
+        public void Message()
+        {
+            Console.WriteLine("Действуй, уже 7!");
+        }
+    }
+}
---
+++ ConsoleApp18/Handler2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class Handler2
+    {
+        public void Message()
+        {
+            Console.WriteLine("Ой, действительно 7!");
+        }
+    }
+}
